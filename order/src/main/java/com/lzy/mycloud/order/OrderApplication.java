e085c186aeca258ae2ea6c374a35ae8acf061cf4
TheBirdies <1184167938@qq.com>
2020-01-13 11:37:15 +0800

    第一次提交

--- order/src/main/java/com/lzy/mycloud/order/OrderApplication.java
+++ order/src/main/java/com/lzy/mycloud/order/OrderApplication.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class OrderApplication {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(OrderApplication.class, args);
     }
 
